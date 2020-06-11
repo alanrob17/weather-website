f1ed6b951eac2820604d17c2fd107d560baf5e35
alanrob17 <alan@robsonmail.com>
2020-06-11 10:24:49 +1000

    Create Heroku setup.

--- public/js/app.js
+++ public/js/app.js
@@ -3,7 +3,7 @@
 console.log('Client side JavaScript is loaded...');
 
 const getForecast = (location) => {
-    const url = 'http://localhost:3000/weather?address=' + location;
+    const url = '/weather?address=' + location;
 
     fetch(url).then((response) => {
         response.json().then((data) => {
--- src/app.js
+++ src/app.js
@@ -15,6 +15,7 @@ const viewsPath = path.join(__dirname, '../templates/views');
 const partialsPath = path.join(__dirname, '../templates/partials');
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 // Setup handlebars engine and views location.
 app.set('view engine', 'hbs');
@@ -103,6 +104,6 @@ app.get('*', (req, res) => {
     });
 });
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000...');
+app.listen(port, () => {
+    console.log('Server is up on port ' + port);
 });
